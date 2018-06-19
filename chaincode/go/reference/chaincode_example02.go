62be1c4b470da0ab974893328bca1e7dff6dd1c6
Dmitry Mihalchenko <dm.mail@tut.by>
2018-06-19 11:16:38 +0300

    Mod. reference chaincode (product struct)

--- chaincode/go/reference/chaincode_example02.go
+++ chaincode/go/reference/chaincode_example02.go
@@ -1,17 +1,27 @@
-
 package main
 
 import (
-	"strconv"
-
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	pb "github.com/hyperledger/fabric/protos/peer"
 	"encoding/pem"
 	"crypto/x509"
 	"strings"
+	"time"
+	"encoding/json"
+	"fmt"
 )
 
 var logger = shim.NewLogger("SimpleChaincode")
+
+type Product struct {
+	ObjectType  string `json:"productObjectType"`
+	Name        string `json:"productName"`
+	Desc        string `json:"productDesc"`
+	State       string `json:"productState"`
+	Org         string `json:"productOrg"`
+	DateCreated time.Time
+	DateUpdated time.Time
+}
 
 // SimpleChaincode example simple Chaincode implementation
 type SimpleChaincode struct {
@@ -19,39 +29,6 @@ type SimpleChaincode struct {
 
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	logger.Debug("Init")
-
-	_, args := stub.GetFunctionAndParameters()
-	var a, b string    // Entities
-	var aVal, bVal int // Asset holdings
-	var err error
-
-	if len(args) != 4 {
-		return pb.Response{Status:403, Message:"Incorrect number of arguments. Expecting 4"}
-	}
-
-	// Initialize the chaincode
-	a = args[0]
-	aVal, err = strconv.Atoi(args[1])
-	if err != nil {
-		return pb.Response{Status:403, Message:"Expecting integer value for asset holding"}
-	}
-	b = args[2]
-	bVal, err = strconv.Atoi(args[3])
-	if err != nil {
-		return pb.Response{Status:403, Message:"Expecting integer value for asset holding"}
-	}
-	logger.Debugf("aVal, bVal = %d", aVal, bVal)
-
-	// Write the state to the ledger
-	err = stub.PutState(a, []byte(strconv.Itoa(aVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(b, []byte(strconv.Itoa(bVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
 
 	return shim.Success(nil)
 }
@@ -69,9 +46,9 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	logger.Debug("transaction creator " + name + "@" + org)
 
 	function, args := stub.GetFunctionAndParameters()
-	if function == "move" {
-		// Make payment of x units from a to b
-		return t.move(stub, args)
+	if function == "add" {
+		// Add product
+		return t.add(stub, args)
 	} else if function == "delete" {
 		// Deletes an entity from its state
 		return t.delete(stub, args)
@@ -80,61 +57,63 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 		return t.query(stub, args)
 	}
 
-	return pb.Response{Status:403, Message:"Invalid invoke function name."}
+	return pb.Response{Status: 403, Message: "Invalid invoke function name."}
 }
 
-// Transaction makes payment of x units from a to b
-func (t *SimpleChaincode) move(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	var a, b string    // Entities
-	var aVal, bVal int // Asset holdings
-	var x int          // Transaction value
+// Transaction makes adding product to org
+func (t *SimpleChaincode) add(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	//var productName, productDesc string // Entities
+	//var product Product
+	//var DateCreated, DateUpdated time.Time
 	var err error
 
-	if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3")
+	if len(args) != 4 {
+		return shim.Error("Incorrect number of arguments. Expecting 4")
 	}
 
-	a = args[0]
-	b = args[1]
+	fmt.Println("Init product")
+	if len(args[0]) <= 0 {
+		return shim.Error("1st argument must be a non-empty string")
+	}
+	if len(args[1]) <= 0 {
+		return shim.Error("2nd argument must be a non-empty string")
+	}
+	if len(args[2]) <= 0 {
+		return shim.Error("3rd argument must be a non-empty string")
+	}
+	if len(args[3]) <= 0 {
+		return shim.Error("4th argument must be a non-empty string")
+	}
 
-	// Get the state from the ledger
-	aBytes, err := stub.GetState(a)
+	productName := args[0]
+	productDesc := args[1]
+	productState := args[2]
+	productOrg := args[3]
+
+	// Check if product already exists
+	productAsBytes, err := stub.GetState(productName)
+	if err != nil {
+		return shim.Error("Failed to get product: " + err.Error())
+	} else if productAsBytes != nil {
+		fmt.Println("This product already exists: " + productName)
+		return shim.Error("This product already exists: " + productName)
+	}
+
+	// Create product object and marshal to JSON
+	DateCreated := time.Now()
+	DateUpdated := time.Now()
+	objectType := "product"
+	product := &Product{objectType, productName, productDesc, productState,
+		productOrg, DateCreated, DateUpdated}
+	productJSONasBytes, err := json.Marshal(product)
+
+	// Write the product to the ledger
+	err = stub.PutState(productName, productJSONasBytes)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	if aBytes == nil {
-		return pb.Response{Status:404, Message:"Entity not found"}
-	}
-	aVal, _ = strconv.Atoi(string(aBytes))
 
-	bBytes, err := stub.GetState(b)
-	if err != nil {
-		return shim.Error("Failed to get state")
-	}
-	if bBytes == nil {
-		return pb.Response{Status:404, Message:"Entity not found"}
-	}
-	bVal, _ = strconv.Atoi(string(bBytes))
-
-	// Perform the execution
-	x, err = strconv.Atoi(args[2])
-	if err != nil {
-		return pb.Response{Status:403, Message:"Invalid transaction amount, expecting an integer value"}
-	}
-	aVal = aVal - x
-	bVal = bVal + x
-	logger.Debug("aVal = %d, bVal = %d\n", aVal, bVal)
-
-	// Write the state back to the ledger
-	err = stub.PutState(a, []byte(strconv.Itoa(aVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
-
-	err = stub.PutState(b, []byte(strconv.Itoa(bVal)))
-	if err != nil {
-		return shim.Error(err.Error())
-	}
+	fmt.Println("End init product")
 
 	return shim.Success(nil)
 }
@@ -142,13 +121,13 @@ func (t *SimpleChaincode) move(stub shim.ChaincodeStubInterface, args []string) 
 // deletes an entity from state
 func (t *SimpleChaincode) delete(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) != 1 {
-		return pb.Response{Status:403, Message:"Incorrect number of arguments"}
+		return pb.Response{Status: 403, Message: "Incorrect number of arguments"}
 	}
 
-	a := args[0]
+	productName := args[0]
 
 	// Delete the key from the state in ledger
-	err := stub.DelState(a)
+	err := stub.DelState(productName)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
@@ -161,27 +140,27 @@ func (t *SimpleChaincode) query(stub shim.ChaincodeStubInterface, args []string)
 	var a string // Entities
 	var err error
 
-	//if len(args) != 1 {
-	//	return pb.Response{Status:403, Message:"Incorrect number of arguments"}
-	//}
+	if len(args) != 1 {
+		return pb.Response{Status:403, Message:"Incorrect number of arguments"}
+	}
 
-	a = args[0]
+	productName = args[0]
 
 	// Get the state from the ledger
-	valBytes, err := stub.GetState(a)
+	productBytes, err := stub.GetState(productName)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 
-	if valBytes == nil {
-		return pb.Response{Status:404, Message:"Entity not found"}
+	if productBytes == nil {
+		return pb.Response{Status: 404, Message: "Entity not found"}
 	}
 
-	return shim.Success(valBytes)
+	return shim.Success(productBytes)
 }
 
-var getCreator = func (certificate []byte) (string, string) {
-	data := certificate[strings.Index(string(certificate), "-----"): strings.LastIndex(string(certificate), "-----")+5]
+var getCreator = func(certificate []byte) (string, string) {
+	data := certificate[strings.Index(string(certificate), "-----") : strings.LastIndex(string(certificate), "-----")+5]
 	block, _ := pem.Decode([]byte(data))
 	cert, _ := x509.ParseCertificate(block.Bytes)
 	organization := cert.Issuer.Organization[0]
