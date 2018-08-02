1b6cf5c71f6e63d3392c4af54b5d32b4d256e13e
Ilya Patotski <ilyapototsky95@gmail.com>
2018-08-02 18:14:47 +0300

    Ownership and product existence checks added to sendRequest function. TransferDetails definition moved to a separate file.

--- chaincode/go/relationship/chaincode_example02.go
+++ chaincode/go/relationship/chaincode_example02.go
@@ -9,157 +9,14 @@ import (
 	"strings"
 	"fmt"
 	"encoding/json"
-	"errors"
-)
-
-const (
-	transferIndex = "TransferDetails"
-)
-
-const (
-	basicArgumentsNumber = 3
-	keyFieldsNumber = 3
-)
-
-const (
-	statusInitiated = "Initiated"
-	statusAccepted = "Accepted"
-	statusRejected = "Rejected"
 )
 
 var logger = shim.NewLogger("OwnershipChaincode")
 
-type TransferDetailsKey struct {
-	ProductKey      string `json:"productKey"`
-	RequestSender   string `json:"requestSender"`
-	RequestReceiver string `json:"requestReceiver"`
-}
-
-type TransferDetailsValue struct {
-	Status  string `json:"status"`
-	Message string `json:"message"`
-}
-
-type TransferDetails struct {
-	Key   TransferDetailsKey   `json:"key"`
-	Value TransferDetailsValue `json:"value"`
-}
-
-func (details *TransferDetails) FillFromArguments(args []string) error {
-	if len(args) < basicArgumentsNumber {
-		return errors.New(fmt.Sprintf("arguments array must contain at least %d items", basicArgumentsNumber))
-	}
-
-	if err := details.FillFromCompositeKeyParts(args[:keyFieldsNumber]); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (details *TransferDetails) FillFromCompositeKeyParts(compositeKeyParts []string) error {
-	if len(compositeKeyParts) < keyFieldsNumber {
-		return errors.New(fmt.Sprintf("composite key parts array must contain at least %d items", keyFieldsNumber))
-	}
-
-	details.Key.ProductKey = compositeKeyParts[0]
-	details.Key.RequestSender = compositeKeyParts[1]
-	details.Key.RequestReceiver = compositeKeyParts[2]
-
-	return nil
-}
-
-func (details *TransferDetails) FillFromLedgerValue(ledgerValue []byte) error {
-	if err := json.Unmarshal(ledgerValue, &details.Value); err != nil {
-		return err
-	} else {
-		return nil
-	}
-}
-
-func (details *TransferDetails) ToCompositeKey(stub shim.ChaincodeStubInterface) (string, error) {
-	compositeKeyParts := []string {
-		details.Key.ProductKey,
-		details.Key.RequestSender,
-		details.Key.RequestReceiver,
-	}
-
-	return stub.CreateCompositeKey(transferIndex, compositeKeyParts)
-}
-
-func (details *TransferDetails) ToLedgerValue() ([]byte, error) {
-	return json.Marshal(details.Value)
-}
-
-func (details *TransferDetails) ExistsIn(stub shim.ChaincodeStubInterface) bool {
-	compositeKey, err := details.ToCompositeKey(stub)
-	if err != nil {
-		return false
-	}
-
-	if data, err := stub.GetState(compositeKey); err != nil || data == nil {
-		return false
-	}
-
-	return true
-}
-
-func (details *TransferDetails) LoadFrom(stub shim.ChaincodeStubInterface) error {
-	compositeKey, err := details.ToCompositeKey(stub)
-	if err != nil {
-		return err
-	}
-
-	data, err := stub.GetState(compositeKey)
-	if err != nil {
-		return err
-	}
-
-	return details.FillFromLedgerValue(data)
-}
-
-func (details *TransferDetails) UpdateOrInsertIn(stub shim.ChaincodeStubInterface) error {
-	compositeKey, err := details.ToCompositeKey(stub)
-	if err != nil {
-		return err
-	}
-
-	value, err := details.ToLedgerValue()
-	if err != nil {
-		return err
-	}
-
-	if err = stub.PutState(compositeKey, value); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (details *TransferDetails) EmitState(stub shim.ChaincodeStubInterface) error {
-	type eventDetails struct {
-		ProductKey string `json:"product_key"`
-		OldOwner   string `json:"old_owner"`
-		NewOwner   string `json:"new_owner"`
-	}
-
-	ed := eventDetails{
-		ProductKey: details.Key.ProductKey,
-		OldOwner: details.Key.RequestReceiver,
-		NewOwner: details.Key.RequestSender,
-	}
-
-	bytes, err := json.Marshal(ed)
-	if err != nil {
-		return err
-	}
-
-	if err = stub.SetEvent(transferIndex + "." + details.Value.Status, bytes); err != nil {
-		return err
-	}
-
-	return nil
-}
+const (
+	commonChannelName = "common"
+	commonChaincodeName = "reference"
+)
 
 // OwnershipChaincode example simple Chaincode implementation
 type OwnershipChaincode struct {
@@ -197,8 +54,6 @@ func (t *OwnershipChaincode) sendRequest(stub shim.ChaincodeStubInterface, args 
 			expectedArgumentsNumber, len(args)))
 	}
 
-	// TODO: check product existence in common channel
-	// TODO: check ownership
 
 	request := TransferDetails{}
 	if err := request.FillFromArguments(args); err != nil {
@@ -218,6 +73,32 @@ func (t *OwnershipChaincode) sendRequest(stub shim.ChaincodeStubInterface, args 
 
 		if request.Value.Status == statusInitiated {
 			return shim.Error("ownership transfer is already initiated")
+		}
+	}
+
+	// TODO: check product existence in common channel
+	// TODO: check ownership
+	type simplifiedProduct struct {
+		Key string `json:"key"`
+		Value struct {
+			Owner string `json:"owner"`
+		} `json:"value"`
+	}
+	response := stub.InvokeChaincode(commonChaincodeName,
+		[][]byte{[]byte("readProduct"), []byte(request.Key.ProductKey)}, commonChannelName)
+	if response.Status >= 400 {
+		return shim.Error(fmt.Sprintf("unable to read product %s from common channel: %s",
+			request.Key.ProductKey, response.Message))
+	} else {
+		var p simplifiedProduct
+		if err := json.Unmarshal(response.Payload, p); err != nil {
+			return shim.Error(fmt.Sprintf("unable to unmarshal response on product %s from common channel",
+				request.Key.ProductKey))
+		}
+
+		if p.Value.Owner != request.Key.RequestReceiver {
+			return shim.Error(fmt.Sprintf("product %s don't belong to organization %s",
+				request.Key.ProductKey, request.Key.RequestReceiver))
 		}
 	}
 
